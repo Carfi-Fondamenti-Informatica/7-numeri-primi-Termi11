172e4116a91d275d6fb27b1b9e8751fce7eb901e
Termi11 <91840662+Termi11@users.noreply.github.com>
2021-11-30 20:22:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+   int n=0;
+    cin>>n;
+    if (numeroprimo(n, n-1)){
+        cout<< "numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
   return 0;
 }
